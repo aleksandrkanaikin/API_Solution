55edaf625c94bfaf15115edfa74ffb678e30c734
aleksandrkanaikin <aleksandrkanajkin7@gmail.com>
2023-09-14 13:08:09 +0300

    Lab 3 complited

--- API_Solution/Startup.cs
+++ API_Solution/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using API_Solution.Extensions;
+using Contracts;
 using Microsoft.AspNetCore.HttpOverrides;
 using NLog;
 
@@ -26,10 +27,11 @@ public class Startup
         services.AddControllers();
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
+        services.AddAutoMapper(typeof(Startup));
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerManager logger)
     {
         if (env.IsDevelopment())
         {
@@ -38,6 +40,7 @@ public class Startup
             app.UseSwaggerUI();
         }
 
+        app.ConfigureExceptionHandler(logger);
         app.UseHttpsRedirection();
         app.UseStaticFiles();
         app.UseCors("CorsPolicy");
--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -10,5 +10,6 @@ namespace Repository
         : base(repositoryContext)
         {
         }
+        public IEnumerable<Company> GetAllCompanies(bool trackChanges) => FindAll(trackChanges).OrderBy(c => c.Name).ToList();
     }
 }
