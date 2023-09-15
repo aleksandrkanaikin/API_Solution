c743eab66876f4beb8a261ef923f7315eeb8cb7e
aleksandrkanaikin <aleksandrkanajkin7@gmail.com>
2023-09-15 20:25:01 +0300

    Lab 5 complited

--- API_Solution/MappingProfile.cs
+++ API_Solution/MappingProfile.cs
@@ -10,6 +10,9 @@ namespace API_Solution
         {
             CreateMap<Company, CompanyDto>().ForMember(c => c.FullAddress, opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
             CreateMap<Employee, EmployeeDto>();
+            CreateMap<CompanyForCreationDto, Company>();
+            CreateMap<EmployeeForCreationDto, Employee>();
+
         }
     }
 }
