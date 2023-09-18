04b40a7b27513ba9278adff5b4a36ae53faa153b
aleksandrkanaikin <aleksandrkanajkin7@gmail.com>
2023-09-18 14:14:53 +0300

    2

--- Contracts/ICompanyRepository.cs
+++ Contracts/ICompanyRepository.cs
@@ -4,6 +4,7 @@ namespace Contracts
 {
     public interface ICompanyRepository
     {
+        public void Delete1(Company company);
         IEnumerable<Company> GetAllCompanies(bool trackChanges);
     }
 }
