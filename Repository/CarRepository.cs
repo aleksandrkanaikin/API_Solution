ac7c6cf101a9682e1b7b8ce28bed8efdce3cc42c
aleksandrkanaikin <aleksandrkanajkin7@gmail.com>
2023-09-22 23:27:08 +0300

    1

--- Repository/CarRepository.cs
+++ Repository/CarRepository.cs
@@ -12,7 +12,7 @@ namespace Repository
         }
 
         public IEnumerable<Car> GetAllCars(bool trackChanges) => FindAll(trackChanges).OrderBy(c=> c.Brend).ToList();
-        public Car GetCarById(Guid driverId, Guid id, bool trackChanges) => FindByCondition(c => c.Id.Equals(id) && 
-                c.
+        public Car GetCarById(Guid driverId, Guid id, bool trackChanges) => FindByCondition(c => c.DriverId.Equals(driverId) &&
+                                c.Id.Equals(id), trackChanges).SingleOrDefault();
     }
 }
